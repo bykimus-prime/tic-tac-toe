27689d894366f352ae4b18744b6d0e714f4e9a16
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2023-02-10 15:22:56 +0900

    Initial logic for placing markers on the gameboard

--- script.js
+++ script.js
@@ -2,14 +2,39 @@
 
 // gameboard module
 let gameBoardModule = (function() { // variable that calls anonymous function
-   let gameBoard = []; // declare empty array
-   return {}; // braces allow us to return an object/funciton to make it public
+   let gameBoard = ["X"]; // declare empty array
+   return {gameBoard}; // braces allow us to return an object/funciton to make it public
 })(); // empty parenthesis immediately invokes function
 
 // display controller module
 let displayControllerModule = (function() {
-   let testVar = () => {console.log('this is a private function call inside a module')};
-   return {testVar};
+   const makeMoveBtn = document.querySelectorAll('.gameboard-btn');
+   
+   // index anad loop through each button node
+   let index = 0;
+   makeMoveBtn.forEach(makeMoveBtns => {
+      makeMoveBtns.dataset.linkedButton = index; // creates data attribute of linkedButton for each .gameboard-btn
+      makeMoveBtns.addEventListener('click', renderArray);
+
+      function renderArray() {
+         const gameGrid = document.querySelectorAll('.cell');
+
+         // index and loop through each grid box node
+         let index = 0;
+         gameGrid.forEach(gameGrids => {
+            gameGrids.dataset.linkedButton = index; // same as above but for each .cell
+
+            // render clicked play on grid cell
+            if (gameGrids.getAttribute('data-linked-button') == makeMoveBtns.getAttribute('data-linked-button')) { //getAttribute searches html, so used dashes for data-linked-button instead of linkedButton
+               gameGrids.textContent = gameBoardModule.gameBoard[gameBoardModule.gameBoard.length - 1]; // goes to most recent array item in gameboard
+               console.log('show me makemoves linked button value: ', makeMoveBtns.dataset.linkedButton);
+               console.log('show gamegrid linked button value: ', gameGrids.dataset.linkedButton);
+            }
+         index++;
+         })
+      }
+   index++;
+   })
 })();
 
 // players factory function
@@ -23,3 +48,19 @@ let playersFactory = (playerName, playerNumber, marker) => {
 
 let alex = playersFactory('Alex', 1, 'X');
 let max = playersFactory('Max', 2, 'O');
+
+displayControllerModule;
+
+// modal logic
+// const openAddBookModal = () => {
+//    addBookModal.classList.add('active');
+//    overlay.classList.add('active');
+// }
+
+// const closeAddBookModal = () => {
+//    addBookModal.classList.remove('active');
+//    overlay.classList.remove('active');
+// }
+
+// addNewBookBtn.onclick = openAddBookModal;
+// overlay.onclick = closeAddBookModal;
