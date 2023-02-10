9ca4fa47ec5792eb0dc120d0b630f6842cb04a77
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2023-02-10 16:28:27 +0900

    Add logic for getting player information and executing gameplay turns

--- script.js
+++ script.js
@@ -1,10 +1,63 @@
 // if you only ever need one of something, use a module. if you need multiples of something, use factories
 
+// players factory function
+let playersFactory = () => {
+
+   // loop through two times to get player's first name, auto assign player number and marker
+   for (let i = 0; i < 4; i++) { // 4 because need some tries to input information correctly before needing to manually try again
+      if (gameBoardModule.playerArray.length >= 6) { // 6 because 3 object attributes for each player
+         gameBoardModule.makePlayerMove();
+         break;
+
+      } else if (gameBoardModule.playerArray.length == 0) { // nothing in playerArray yet
+         let playerName = prompt('What\'s your name?');
+         if (playerName == '' || playerName == null) {
+            alert('Name can\'t be blank.');
+            continue;
+         }
+         let playerNumber = 1;
+         let marker = 'X';
+         alert('You are Player 1, your marker is X');
+         gameBoardModule.playerArray.push(playerName, playerNumber, marker);
+
+      } else if (gameBoardModule.playerArray.length !== 0) {
+         let playerName = prompt('What\'s your name?');
+         if (playerName == '' || playerName == null) {
+            alert('Name can\'t be blank.');
+            continue;
+         }
+         let playerNumber = 2;
+         let marker = 'O';
+         alert('You are Player 2, your marker is O');
+         gameBoardModule.playerArray.push(playerName, playerNumber, marker);
+      }
+   }
+};
+
 // gameboard module
-let gameBoardModule = (function() { // variable that calls anonymous function
-   let gameBoard = ["X"]; // declare empty array
-   return {gameBoard}; // braces allow us to return an object/funciton to make it public
-})(); // empty parenthesis immediately invokes function
+let gameBoardModule = (function() {
+   let gameBoard = [];
+   let playerArray = [];
+
+   // public function to make next player move
+   let makePlayerMove = () => {
+      // check for two player submission and gameboard array doesn't spill over grid cells
+      if (playerArray.length == 6 && gameBoard.length < 9) { // 6 means all 3 attributes for each player is loaded
+         // controls for player moves
+         if (gameBoard.length == 0) { // first play of the game
+            alert('Player 1\'s move.')
+            gameBoard.push(playerArray[2]); // item 2 is marker for player 1, X
+         } else if (gameBoard[gameBoard.length - 1] == 'X') { // most recent far right item in array
+            alert('Player 2\'s move.');
+            gameBoard.push(playerArray[5]); // item 5 is marker for player 2, O
+         } else if (gameBoard[gameBoard.length - 1] == 'O') {
+            alert('Player 1\'s move.');
+            gameBoard.push(playerArray[2]);
+         }
+      }
+   }
+   return {gameBoard, playerArray, makePlayerMove};
+})();
 
 // display controller module
 let displayControllerModule = (function() {
@@ -27,29 +80,19 @@ let displayControllerModule = (function() {
             // render clicked play on grid cell
             if (gameGrids.getAttribute('data-linked-button') == makeMoveBtns.getAttribute('data-linked-button')) { //getAttribute searches html, so used dashes for data-linked-button instead of linkedButton
                gameGrids.textContent = gameBoardModule.gameBoard[gameBoardModule.gameBoard.length - 1]; // goes to most recent array item in gameboard
-               console.log('show me makemoves linked button value: ', makeMoveBtns.dataset.linkedButton);
-               console.log('show gamegrid linked button value: ', gameGrids.dataset.linkedButton);
             }
          index++;
          })
+      gameBoardModule.makePlayerMove(); // each time btn is clicked 
       }
    index++;
    })
+   // listen for click to start game
+   const startGameBtn = document.querySelector('.start-game-btn');
+   startGameBtn.addEventListener('click', playersFactory);
+   return {};
 })();
 
-// players factory function
-let playersFactory = (playerName, playerNumber, marker) => {
-   let getPlayerName = () => {
-      playerName;
-      console.log('player' + playerNumber +'\'s name is: ' + playerName);
-   }
-   return {getPlayerName, playerName, playerNumber, marker};
-};
-
-let alex = playersFactory('Alex', 1, 'X');
-let max = playersFactory('Max', 2, 'O');
-
-displayControllerModule;
 
 // modal logic
 // const openAddBookModal = () => {
